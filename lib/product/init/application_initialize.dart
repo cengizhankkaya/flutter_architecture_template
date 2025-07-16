ad991bb764419e64c7dfc31a0eed335e2d4bfbf8
Cengizhan Kaya <cengizhan.kaya.1453@gmail.com>
2025-07-16 11:14:05 +0300

    project initialize added on main

---
+++ lib/product/init/application_initialize.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_logger/easy_logger.dart';
+import 'package:flutter/services.dart';
+import 'package:kartal/kartal.dart';
+import 'package:logger/web.dart';
+
+@immutable
+/// This class is responsible for initializing the application.
+final class ApplicationInitialize {
+  Future<void> make() async {
+    await runZonedGuarded<Future<void>>(
+      _initialize,
+      (error, stackTrace) {
+        ///  Todo: add custom logger
+        Logger().e(error.toString());
+      },
+    );
+  }
+
+  /// Initializes the application.
+  static Future<void> _initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await EasyLocalization.ensureInitialized();
+    EasyLocalization.logger.enableLevels = [
+      LevelMessages.error,
+      LevelMessages.warning,
+    ];
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    // Additional initialization logic can be added here.
+    await DeviceUtility.instance.initPackageInfo();
+
+    FlutterError.onError = (errorDetails) {
+      /// crashlytics log insert here
+      /// custom service pr custom logger insert here
+      ///  Todo: add custom logger
+      Logger().e(errorDetails.exceptionAsString());
+    };
+
+    ///  Dependency Initialize
+    /// envied
+  }
+}
